54f6211b061ffd6abfb54a01b47116132158cb11
Artiom <artemmd2009@gmail.com>
2023-08-15 22:30:49 +0200

    design

--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -44,7 +44,7 @@
 
                   <div>
                     <button type="submit"
-                      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mb-2 rounded w-full">
+                      class="bg-green-500 mr-2 hover:bg-green-700 text-white font-bold py-2 px-4 mb-2 rounded w-full">
                       Update
                     </button>
                   </div>
