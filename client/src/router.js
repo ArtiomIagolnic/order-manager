9558db41dbb9eb9647467fe0a08fa5a90fd8c8be
Artiom <artemmd2009@gmail.com>
2023-09-05 15:42:10 +0200

    package

--- client/src/router.js
+++ client/src/router.js
@@ -1,39 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AppCustomersList from "@/pages/customers/index.vue";
-import AppProductsList from "@/pages/products/index.vue";
-import AppOrdersList from "@/pages/orders/index.vue";
-import AppE404 from "@/components/E404.vue";
-import AppHomeComponent from "@/pages/Home.vue";
-
 const routes = [
   {
     name: "home",
     path: "/",
-    component: AppHomeComponent,
+    component: () => import(/* webpackChunkName: "home" */ "./pages/Home.vue"),
   },
   {
     name: "customers",
     path: "/customers",
-    component: AppCustomersList,
+    component: () =>
+      import(/* webpackChunkName: "customers" */ "./pages/customers/index.vue"),
   },
   {
     name: "products",
     path: "/products",
-    component: AppProductsList,
+    component: () =>
+      import(/* webpackChunkName: "products" */ "./pages/products/index.vue"),
   },
   {
     name: "orders",
     path: "/orders",
-    component: AppOrdersList,
+    component: () =>
+      import(/* webpackChunkName: "orders" */ "./pages/orders/index.vue"),
   },
   {
     path: "/:any(.*)",
-    component: AppE404
-  }
+    component: () => import(/* webpackChunkName: ""*/ "./components/E404.vue"),
+  },
 ];
 
 export default createRouter({
   routes,
-  history: createWebHistory()
+  history: createWebHistory(),
 });
