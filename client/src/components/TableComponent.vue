0f6880225b4efcbe89b0c1f7ebf9ce8a502e4e1c
Artiom <artemmd2009@gmail.com>
2023-09-20 13:42:49 +0200

    mobile cards updated

--- client/src/components/TableComponent.vue
+++ client/src/components/TableComponent.vue
@@ -1,39 +1,36 @@
 <template >
-    <!-- mobile version -->
     <div class="container">
-        <div v-if="windowWidth < 640">
-            <table v-for="(item, i) in items" :key="item.id"
-                class="w-full flex flex-row flex-no-wrap rounded-lg overflow-hidden my-5">
+        <!-- mobile version -->
+        <div class="bg-white md:hidden ">
+            <div class="space-y-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <!-- Loop Through Items -->
+                <div v-for="(item, i) in items" :key="item.id" class="bg-white shadow-md rounded-lg overflow-hidden">
+                    <div class="px-6 py-4">
+                        <div class="flex items-center justify-between">
+                            <div class="text-lg font-bold text-teal-400">#{{ i + 1 }}</div>
 
-                <thead class="text-white w-1/2">
-                    <tr class="bg-teal-400 flex flex-col flex-no wrap rounded-l-lg mb-2">
-                        <th class="p-2 text-left text-m">Nr.</th>
-                        <th v-for="header, index in headers" :key="index" class="p-2 text-left text-m">{{ header }}</th>
-                        <th class="p-2 text-left text-m" width="110px">Actions</th>
-                    </tr>
-                </thead>
-
-                <tbody class="flex-1 w-1/2">
-                    <tr class="flex flex-col flex-no wrap mb-2">
-                        <td class="border-grey-light border hover:bg-gray-100 p-2">{{ i + 1 }}</td>
-                        <td v-for="prop, index in itemProps" :key="index"
-                            class="border-grey-light border hover:bg-gray-100 p-2">{{ item[prop] }}</td>
-
-                        <td class="border-grey-light border hover:bg-gray-100 p-2 flex justify-around">
+                        </div>
+                        <template v-for="(header, index) in headers" :key="index">
+                            <div class="mt-2">
+                                <div class="text-gray-700 font-extrabold">{{ header }}:</div>
+                                <div class="text-gray-900">{{ item[itemProps[index]] }}</div>
+                            </div>
+                        </template>
+                        <div class="mt-3 space-x-4 flex justify-start">
                             <button @click="emitUpdate(item)"
-                                class="text-blue-400 hover:text-blue-600 hover:font-medium cursor-pointer">Update</button>
+                                class="text-blue-600 hover:text-blue-800 hover:font-medium cursor-pointer">Update</button>
                             <button @click="emitDelete(item)"
-                                class="text-red-400 hover:text-red-600 hover:font-medium cursor-pointer">Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                                class="text-red-600 hover:text-red-800 hover:font-medium cursor-pointer">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Desktop version -->
-        <div v-else>
-
-            <table class="w-full flex flex-row flex-no-wrap bg-white rounded-lg overflow-hidden shadow-lg my-5">
+        <div class="hidden md:block">
+            <table
+                class="desktop-table w-full border-collapse border border-gray-200 rounded-lg overflow-hidden shadow-lg my-5">
                 <thead class="text-white">
                     <tr class="bg-teal-400 flex-col flex-no wrap table-row rounded-l-lg rounded-none mb-0">
                         <th class="p-3 text-left">Nr</th>
@@ -79,54 +76,22 @@ export default {
             default: []
         }
     },
-    data() {
-        return {
-            windowWidth: window.innerWidth
-        }
-    },
-    mounted() {
-        this.$nextTick(() => {
-            window.addEventListener('resize', this.onResize);
-        })
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize);
-    },
+
     methods: {
         emitUpdate(item) {
             this.$emit('update-item', item)
         },
         emitDelete(item) {
             this.$emit('delete-item', item)
-        },
-        onResize() {
-            this.windowWidth = window.innerWidth
         }
     }
 }
 </script>
 
 <style>
-html,
-body {
-    height: 100%;
-}
-
-@media (min-width: 640px) {
-    table {
+@media (min-width: 767px) {
+    .desktop-table {
         display: inline-table !important;
     }
-
-    thead tr:not(:first-child) {
-        display: none;
-    }
-}
-
-td:not(:last-child) {
-    border-bottom: 0;
-}
-
-th:not(:last-child) {
-    border-bottom: 2px solid rgba(0, 0, 0, .1);
 }
 </style>
